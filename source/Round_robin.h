705d4ffd81fa18d0c834f31b7e13f162bf9fc4bc
Momitul Hoque Chowdhury <110973021+Momit87@users.noreply.github.com>
2024-06-24 22:40:28 +0600

    Add files via upload

---
+++ source/Round_robin.h
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void show4(vector<int> tim,vector<char> pro)
+{   
+   
+    string s, p;
+    s = "Time   :";
+    p = "Process:";
+    int n = tim.size();
+    for (int i = 0; i < n; i++)
+    {
+        string spa = to_string(tim[i]);
+        s += ' ';
+        s += spa;
+
+        for (int j = 0; j < spa.size(); j++)
+        {
+            p += ' ';
+        }
+        p += pro[i];
+    }
+
+    cout << "Gantt chart-----\n";
+    cout << s << endl;
+    cout << p << endl;
+    return;
+}
+int Round_robin(int n,vector<pair<int,int>>anb,int cpu_time)
+{
+    
+    vector<pair<pair<int, int>, char>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+       
+        v[i].first.first=anb[i].first;
+        v[i].first.second=anb[i].second;
+        v[i].second = 'A' + i;
+    }
+    sort(v.begin(), v.end());
+    
+    queue<pair<int, char>> ready;
+    int time = cpu_time;
+    int j = 0;
+
+    while (j < n && v[j].first.first <= time)
+    {
+        ready.push({v[j].first.second, v[j].second});
+
+        j++;
+    }
+    // cout << "----------------Gantt Chart-----------------" << endl;
+    // cout << "Time  : Character " << endl;
+    vector<int>tim;
+    vector<char>pro;
+    while (!ready.empty())
+    {
+        pair<int, char> temp = ready.front();
+        ready.pop();
+        int execute = min(temp.first, cpu_time);
+        temp.first -= execute;
+
+        time += execute;
+        // cout << time << ' ' << temp.second << endl;
+        tim.push_back(time);
+        pro.push_back(temp.second);
+        while (j < n && v[j].first.first <= time)
+        {
+            ready.push({v[j].first.second, v[j].second});
+
+            j++;
+        }
+
+        if (temp.first > 0)
+        {
+            ready.push(temp);
+        }
+    }
+    show4(tim,pro);
+    cout<<endl;
+    cout << "Round Robin"<< " took " << time << " Units time" << endl;
+    cout<<endl;
+    cout<<endl;
+    // cout<<"Times : "<<time<<endl;
+    return time;
+}
